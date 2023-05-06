6f4e228de6376c357b14b2c9078e65a8cab2e5f9
NARizzardi <rizzanic2000@gmail.com>
2023-05-06 18:41:52 -0300

    [add] temporary files
    Signed-off-by: NARizzardi <rizzanic2000@gmail.com>

---
+++ tmp/AreaProibida.java
@@ -0,0 +1,4 @@
+
+public class AreaProibida extends Entidade{
+
+}
---
+++ tmp/Entidade.java
@@ -0,0 +1,15 @@
+public abstract class Entidade {
+    protected Posicao posicao;
+
+    public Entidade() {
+        
+    }
+
+    public Posicao getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(Posicao posicao) {
+        this.posicao = posicao;
+    }
+}
