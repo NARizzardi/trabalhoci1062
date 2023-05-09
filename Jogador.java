72d1149f4cb9c4e13adc4cb9d3f9e09efb6c977e
TSenff <None>
2023-05-09 01:34:15 -0300

    [ADD] Jogador incompleto

--- Jogador.java
+++ Jogador.java
@@ -1,8 +1,25 @@
 public class Jogador extends Entidade{
     private String nome;
+    private Item item;
+    private boolean boato_flag;
 
-    public Jogador(String nome) {
-        this.nome = nome;
+    public Jogador(String nome,Posicao posicao) {
+        super(posicao);
+        this.setNome(nome);
+        this.boato_flag = false;
+        this.item = null;
+    }
+
+    public void setItem(Item item){
+        this.item = item;
+    }
+
+    public boolean isBoatoFlag(){
+        return this.boato_flag;
+    }
+
+    public void setBoatoFlag(boolean boato_flag){
+        this.boato_flag = boato_flag;
     }
 
     public String getNome() {
@@ -12,6 +29,69 @@ public class Jogador extends Entidade{
     public void setNome(String nome) {
         this.nome = nome;
     }
+    /*
+    *   Movimenta o Personagem 1 casa nas direçoes cardinais
+    *   Valores de direcao (Not case sensitive): 
+    *       w :: Move para o Norte
+    *       a :: Move para o Oeste
+    *       s :: Move para o Sul
+    *       d :: Move para o Leste
+    *   Outro :: Erro
+    *   Retorna uma posição apos o movimento ou null em caso de  erro 
+    */
+    public Posicao movimentoBase(char direcao){
+        Posicao p = this.getPosicao();
+        switch (direcao){
+            case 'D':
+            case 'd':
+                return new Posicao(p.getPosX()+1, p.getPosY());
+            case 'A':
+            case 'a':
+                return new Posicao(p.getPosX()-1, p.getPosY());
+            case 'W':
+            case 'w':
+                return new Posicao(p.getPosX(), p.getPosY()+1);
+            case 'S':
+            case 's':
+                return new Posicao(p.getPosX(), p.getPosY()-1);
+        }
+        return null;
+    }
 
+
+    /* 
+    *   Função para usar o item do personagem, depois de uso jogador perde o item 
+    */
+    public void utilizarItem(/* Recebe o necessario para a função de usar item */){
+        if(this.item != null){
+            /* Chama a função de usar o item */
+            this.item = null;
+        }
+    }
     
+    /*
+    *   Devolve um numero que identifica o tipo do item que o jogador tem
+    *   Retorna
+    *      -1 :: Erro
+    *       0 :: Não tem item 
+    *       1 :: Denuncia 
+    *       2 :: Noticia
+    *       3 :: Boato 
+    *       4 :: Fuga 
+    */
+    public int ChecarItem(){
+        if(this.item == null)
+            return 0;
+        if(this.item instanceof Denuncia)
+            return 1;
+        if(this.item instanceof Noticia)
+            return 2;
+        if(this.item instanceof Boato)
+            return 3;
+        if(this.item instanceof Fuga)
+            return 4;
+        return -1;
+    }
+
+
 }
